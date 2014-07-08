62972c7d9aad6b8a3af6b7984b51414a7ee9c0c2
Christopher Brown <io@henrian.com>
2014-07-08 13:37:43 -0500

    Check for an ended feed; click 'More stories' if desired.

--- extension/feed-content.js
+++ extension/feed-content.js
@@ -1,5 +1,8 @@
 /*jslint browser: true */
-// Chrome ID: ekbecjpljlheelnfeldblankfmapljie
+// Chrome ID: hmfpcpkjkhfegmageccfkekcdbfdffdn
+
+var log = console.log.bind(console);
+// var log = function() {};
 
 var repression_origin = 'https://www.computationalx.com';
 document.addEventListener('DOMContentLoaded', function(event) {
@@ -8,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
   // get username from nav bar
   var timeline_anchor = document.querySelector('.navLink[title=Timeline]');
   window.username = timeline_anchor.pathname.slice(1); // slice off the slash
-  // console.log('username = %s', window.username);
+  log('feed-content: username = %s', window.username);
 
   // add iframe
   var iframe = document.createElement('iframe');
@@ -18,19 +21,29 @@ document.addEventListener('DOMContentLoaded', function(event) {
   document.body.appendChild(iframe);
   window.repression_frame = iframe;
 
+  // just to make sure set up a timeout
+  var initializeMutationObserver_timeout = setTimeout(initializeMutationObserver, 500);
   // get mutation observation target
-  var target = document.getElementById('contentArea');
   window.repression_frame.addEventListener('load', function() {
-    // console.log('iframe ready; watching for changes:', target);
-    new MutationObserver(function(mutations, observer) {
-      window.requestAnimationFrame(repressNewUserContent);
-    }).observe(target, {
-      childList: true,
-      subtree: true,
-    });
+    clearTimeout(initializeMutationObserver_timeout);
+    initializeMutationObserver();
   }, true);
-
+  // and kick it off here, also just to make sure
+  setTimeout(repressNewUserContent, 500);
+  // also, make sure that they never spend too long waiting for content
+  setInterval(ensureEndlessFeed, 500);
 });
+
+var initializeMutationObserver = function() {
+  log('feed-content: initializing mutation_observer');
+  var target = document.getElementById('contentArea');
+  window.mutation_observer = new MutationObserver(function(mutations, observer) {
+    window.requestAnimationFrame(repressNewUserContent);
+  }).observe(target, {
+    childList: true,
+    subtree: true,
+  });
+ };
 
 var showElementById = function(element_id) {
   var el = document.getElementById(element_id);
@@ -41,6 +54,7 @@ var showElementById = function(element_id) {
 
 window.addEventListener('message', function(ev) {
   // the messages that the userscript will receive are simple: just IDs of elements to reveal.
+  log('feed-content: message', ev.data, 'from', ev.origin);
   if (ev.origin == repression_origin) {
     showElementById(ev.data);
   }
@@ -50,28 +64,45 @@ var repressNewUserContent = function() {
   var posts = []; // array of {id: '_0_someElementId', content: 'Look, a kitten!'} objects
 
   // .mbm denotes the container level
-  var post_containers = document.querySelectorAll('._5pcb .mbm:not(.repressed)');
+  var post_containers = document.querySelectorAll('._5pcb .mbm:not(.processed)');
+  if (post_containers.length) {
+    log('feed-content: evaluating %d new posts', post_containers.length);
+    for (var i = 0, post_container; (post_container = post_containers[i]); i++) {
+      // mark the post as processed
+      post_container.classList.add('processed');
 
-  for (var i = 0, post_container; (post_container = post_containers[i]); i++) {
-    // mark the post as processed
-    post_container.classList.add('repressed');
+      // find the actual text content of the post
+      var content_el = post_container.querySelector('.userContent');
+      var content = content_el ? content_el.textContent : '';
+      if (content) {
+        // only send off non-empty posts
+        var author_el = post_container.querySelector('.fwb'); // .profileLink doesn't always catch it
+        var author = author_el ? author_el.textContent : '';
 
-    // find the actual text content of the post
-    var content_el = post_container.querySelector('.userContent');
-    var content = content_el ? content_el.textContent : '';
-    if (content) {
-      // only send off non-empty posts
-      var author_el = post_container.querySelector('.fwb'); // .profileLink doesn't always catch it
-      var author = author_el ? author_el.textContent : '';
-
-      posts.push({id: post_container.id, content: content, author: author});
+        posts.push({id: post_container.id, content: content, author: author});
+      }
+      else {
+        showElementById(post_container.id);
+      }
     }
-    else {
-      showElementById(post_container.id);
+
+    if (posts.length) {
+      // setTimeout(function()
+      window.repression_frame.contentWindow.postMessage(posts, repression_origin);
     }
   }
+};
 
-  if (posts.length) {
-    window.repression_frame.contentWindow.postMessage(posts, repression_origin);
+// hack: check that the "More Stories" button is not visible.
+// if it is, click it!
+var ensureEndlessFeed = function() {
+  var more_stories = document.querySelector('._5usd');
+  if (more_stories) {
+    var more_stories_top = more_stories.getBoundingClientRect().top;
+    // oddly, the more_stories button can get set to have a top of 0
+    if (more_stories_top > 1 && more_stories_top < window.innerHeight) {
+      log('feed-content: requesting more stories (%d < %d)', more_stories_top, window.innerHeight);
+      more_stories.dispatchEvent(new MouseEvent('click'));
+    }
   }
 };
