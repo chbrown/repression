cd7ba2a8a4ebb2944d8ce8536e3ee50439346a17
Christopher Brown <io@henrian.com>
2014-07-09 19:54:20 -0500

    Don't depend on Node.js for load-balancing

--- cluster.js
+++ cluster.js
@@ -5,6 +5,9 @@ var cluster = require('cluster');
 var logger = require('loge');
 var path = require('path');
 
+var config = require('./package').config;
+var port_zero = parseInt(process.env.npm_config_port || config.port, 10);
+
 var optimist = require('optimist')
   .usage('Usage: node cluster.js [options]')
   .describe({
@@ -21,13 +24,15 @@ cluster.on('fork', function(worker) {
   logger.debug('cluster: worker[%d] fork (pid=%d)', worker.id, worker.process.pid);
 });
 cluster.on('disconnect', function(worker) {
-  // disconnect occurs slightly before exit, I think. I'm not sure. anyways, they both happen.
-  logger.error('cluster: worker[%d] disconnect (pid=%d)', worker.id, worker.process.pid);
+  // disconnect occurs slightly before exit.
+  logger.error('cluster: worker[%d] disconnect (pid=%d)',
+    worker.id, worker.process.pid);
 });
 cluster.on('exit', function(worker, code, signal) {
-  logger.error('cluster: worker[%d] exit code=%d signal=%s (pid=%d)', worker.id, code, signal, worker.process.pid);
+  logger.error('cluster: worker[%d] exit code=%d signal=%s (pid=%d)',
+    worker.id, code, signal, worker.process.pid);
   // fork new worker to replace dead one
-  cluster.fork();
+  cluster.fork({npm_config_port: worker.port});
 });
 cluster.on('listening', function(worker, address) {
   logger.debug('cluster: worker[%d] listening %s @ %s:%s (pid=%d)', worker.id,
@@ -37,5 +42,6 @@ cluster.on('listening', function(worker, address) {
 // fork initial workers
 logger.info('Starting cluster with %d forks', argv.forks);
 for (var i = 0; i < argv.forks; i++) {
-  cluster.fork();
+  var worker = cluster.fork({npm_config_port: port_zero + i});
+  worker.port = port_zero + i;
 }
